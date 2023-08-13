e9cf4d34cd939b0d3770bfdd5ff863eb0037014a
sivaraman <ssivaraman122@gmail.com>
2023-08-13 15:55:16 +0530

    adding requirements.txt

--- modules/dynamic_controller.py
+++ modules/dynamic_controller.py
@@ -10,14 +10,18 @@ class DynamicHttpController:
 
     def on_get(self, req, resp):
         if "GET" not in self.methods:
-            raise Exception("Method not supported on resource")
+            resp.status = falcon.HTTP_403
+            resp.message = "Method not supported"
+            return
 
         resp.status = falcon.HTTP_200
         resp.media = self.repo.get_by_key(self.id)
 
     def on_post(self, req, resp):
         if "POST" not in self.methods:
-            raise Exception("POST Method not supported on resource")
+            resp.status = falcon.HTTP_403
+            resp.message = "Method not supported"
+            return
 
         data = json.loads(req.bounded_stream.read().decode('utf-8'))
         resp.status = falcon.HTTP_201
